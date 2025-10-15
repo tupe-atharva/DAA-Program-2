7263d3b493b235afd3e2bc942beaef50776ef674
tupe-atharva <atharva.tupe007@gmail.com>
2025-10-15 07:43:43 -0400

    Created skeleton for LIM using 2 algos

--- tupe_A_pa2_lim.cpp
+++ tupe_A_pa2_lim.cpp
@@ -2,10 +2,46 @@
 #include<vector>
 #include<math.h>
 using namespace std;
- 
-int main(){ 
- 
- 
- 
+
+int generateRandomNumbers(int n ){
+
+
+    return ;
+}
+
+
+void orignalLargeIntMul (int a, int b){
+
+}
+
+void modifiedLargeIntMul(int a, int b){
+
+}
+
+
+
+int main(int argc, char*argv[]){ 
+    int n = stoi(argv[1]); // string to integer conversion of the command line input
+
+    if (argc != 2){
+        cout << "This program expects a single input along with the file name in the command line, please abide to the same to avoid crashes and errors" << endl;
+        return 0;
+    }
+
+    if(n%2 !=0 || n%3 != 0){
+        cout << "Enter the value of n of the order 6k, ie. a number divisble by both 2 and 3" << endl;
+        return 0;
+    }
+
+    // Call function to generate 2 n digit random numbers
+    int a = generateRandomNumbers(n);
+    int b = generateRandomNumbers(n);
+
+    // Call function to calculate LIM using orignal algorithm -> n => n/2
+    orignalLargeIntMul(a,b);
+
+    // Call function to calculate LIM using modified algorithm -> n => n/3
+    modifiedLargeIntMul(a,b);
+
     return 0;
 }
